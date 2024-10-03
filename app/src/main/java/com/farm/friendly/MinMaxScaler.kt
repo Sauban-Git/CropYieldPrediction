bb738d20d326811286c70aec8409c99699c36b41
Sauban-Git <128286356+Sauban-Git@users.noreply.github.com>
2024-10-03 19:33:31 +0530

    implented  model with server req

--- app/src/main/java/com/farm/friendly/MinMaxScaler.kt
+++
@@ -1,7 +0,0 @@
-package com.farm.friendly
-
-class MinMaxScaler (private val min: Double, private val max: Double) {
-    fun scale(value: Double, featureMin: Double,featureMax: Double):Double {
-        return (value - featureMin) / (featureMax - featureMin) * (max - min) + min
-    }
-}
